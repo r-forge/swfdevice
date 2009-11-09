42524d19711796a0b743bd894602b5aee5671743
cameronbracken (Cameron Bracken) <cameron.bracken@gmail.com>
2009-11-09 11:25:22 +0000

    Gallery Update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/swfdevice@26 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/generateGallery.R
+++ www/generateGallery.R
@@ -1,15 +1,5 @@
 #!/usr/bin/env Rscript
 
-file.remove('swfs.html')
 download.file('http://r-forge.r-project.org/plugins/scmsvn/viewcvs.php/*checkout*/pkg/inst/tests/testSwfDevice.R?root=swfdevice',
 	'testSwfDevice.R')
 source('testSwfDevice.R')
-swfs <- list.files(,".swf")
-pngs <- list.files(,".png")
-file.create('swfs.html')
-
-for(i in 1:length(swfs)){
-	system(paste('swfdump --xhtml',swfs[i],'>> swfs.html'))
-	cat('<img src=\'',pngs[i],'\'/>\n',file='swfs.html',append=T,sep='')
-	cat('<br/>\n',file='swfs.html',append=T)
-}
